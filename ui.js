21443cf5ebda3f1526da689e17df6c226b74130b
Eduard Toma <eduard.daniel.toma@gmail.com>
2019-04-11 17:47:19 +0300

    css, icon, logo, animation

--- ui.js
+++ ui.js
@@ -8,9 +8,10 @@ class UI {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
         <div class="row">
+
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-outline-success btn-block btn-sm mb-1">View Profile</a>
+            <a href="${user.html_url}" target="_blank" class="btn btn-outline-success btn-block btn-sm mb-1 p-1">View Profile</a>
           </div>
 
           <div class="col-md-9">
@@ -26,6 +27,7 @@ class UI {
               <li class="list-group-item">Member Since: ${user.created_at}</li>
             </ul>
           </div>
+
         </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
@@ -41,6 +43,7 @@ class UI {
       output += `
         <div class="card card-body mb-3">
           <div class="row">
+
             <div class="col-md-6">
               <a href="${repo.html_url}" target="_blank">${repo.name}</a>
             </div>
@@ -49,6 +52,7 @@ class UI {
             <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
             <span class="badge badge-secondary">Forks: ${repo.forms_count}</span>
             </div>
+            
           </div>
         </div>
       `;
